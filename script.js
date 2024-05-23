cc4f2338eaa5c090c39e3172c958813213c18d74
SowbarnikaArumugam <sowmipavi02@gmail.com>
2024-05-23 08:16:25 +0530

    completed

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ document.addEventListener('DOMContentLoaded',function(){
   
   contactForm.reset();
 
-  display();
+  displayContact();
 
   });
-  function display(){
+  function displayContact(){
     const rightbox = document.querySelector('.right-box');
-    rightbox.innerHTML = '';
+    rightbox.innerHTML = '<h1>Contact Details</h1>';
     
     let contacts = JSON.parse(localStorage.getItem('contacts')) || [];
     
@@ -32,11 +32,37 @@ document.addEventListener('DOMContentLoaded',function(){
         contactdiv.classList.add('contact-item');
         contactdiv.innerHTML = `
             <p>Name: ${contact.name}</p>
-            <p>Number:: ${contact.number}</p> `;
+            <p>Number:${contact.number}</p>
+            <button  onclick="renameContact(${index})">Rename</button>
+            <button onclick="deleteContact(${index})">Delete</button> `;
     
     rightbox.appendChild(contactdiv);
     });
   }
-   display();
+  
+  window.renameContact =  function(index){
+    let contacts = JSON.parse(localStorage.getItem('contacts')) ||[];
+    const contact = contacts[index];
+    let newname =  prompt("Enter new name:", contact.name);
+    if (newname){
+       contact.name = newname;
+       contacts[index] = contact;
+       localStorage.setItem('contacts', JSON.stringify(contacts,null,2));
+    
+    displayContact();
+    }
+  };
+  
+  window.deleteContact = function(index){
+    let contacts = JSON.parse(localStorage.getItem('contacts')) || [];
+    const contact = contacts[index];
+    contacts.splice(index,1);
+    localStorage.setItem('contacts', JSON.stringify(contacts,null,2));
+    displayContact();
+  };
+
+
+
+   displayContact();
 
 });
